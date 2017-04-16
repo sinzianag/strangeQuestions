d7170f0992ee1d17ed523ad7d1f764889939b3e2
Sinziana Gafitanu <sinziana.gafitanu@gmail.com>
2017-04-15 23:24:08 -0700

    Adding home in a new file
    Creating home.dart with the code form main.dart.
    
     Changes to be committed:
    	new file:   lib/home.dart
    	modified:   lib/main.dart

---
+++ lib/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Text(
+          'Button clicked $_counter time${ _counter == 1 ? '' : 's' }.',
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.arrow_forward),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
