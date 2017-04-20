109de9ab15c158820f1c21ed4cc27560d54524fe
Sinziana Gafitanu <sinziana.gafitanu@gmail.com>
2017-04-19 22:58:00 -0700

    Setting up the Column for the App title
    Also changing the color of the app bar and adding a menu to that as well.
    
    Changes to be committed:
    	modified:   lib/home.dart
    	modified:   lib/settings.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 enum _QuestionMenuItem { how, about }
@@ -28,24 +29,36 @@ class _MyHomePageState extends State<MyHomePage> {
     //TODO
   }
 
+  TextStyle _getTextStyle() {
+    return new TextStyle(
+      color: Colors.white,
+      decoration: TextDecoration.none,
+      fontWeight: FontWeight.normal,
+      fontStyle: FontStyle.normal,
+      fontSize: 36.0,
+    );
+  }
+
+
+
   Widget buildAppBar() {
     return new AppBar(
         elevation: 0,
-        backgroundColor: new Color(0xFF263248),
+        backgroundColor: new Color(0xFF1c283d),
         actions: <Widget>[
           new PopupMenuButton<_QuestionMenuItem>(
             onSelected: (_QuestionMenuItem value) {
               _handleMenu(context, value);
             },
             itemBuilder: (BuildContext context) =>
-                <PopupMenuItem<_QuestionMenuItem>>[
-                  new PopupMenuItem<_QuestionMenuItem>(
-                      value: _QuestionMenuItem.how,
-                      child: const Text('How this works')),
-                  new PopupMenuItem<_QuestionMenuItem>(
-                      value: _QuestionMenuItem.about,
-                      child: const Text('About'))
-                ],
+            <PopupMenuItem<_QuestionMenuItem>>[
+              new PopupMenuItem<_QuestionMenuItem>(
+                  value: _QuestionMenuItem.how,
+                  child: const Text('How this works')),
+              new PopupMenuItem<_QuestionMenuItem>(
+                  value: _QuestionMenuItem.about,
+                  child: const Text('About'))
+            ],
           ),
         ]);
   }
@@ -55,16 +68,26 @@ class _MyHomePageState extends State<MyHomePage> {
     return new Scaffold(
       appBar: buildAppBar(),
       body: new Center(
-        child: new Text(
-          'Strange Questions',
-          style: new ThemeData.dark().textTheme.title,
-        ),
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget> [ new Text(
+                '  Strange\nQuestions',
+                style: _getTextStyle(),
+                ),
+                new Divider(height: 30.0, indent: 80.0, color: new Color(0xFF263248)),
+                new FlatButton(onPressed: _handleShowSettings,
+                    child: new Icon(Icons.arrow_forward,
+                    color: Colors.white),
+                )
+              ],
+            ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _handleShowSettings,
-        tooltip: 'Increment',
-        child: new Icon(Icons.arrow_forward),
-      ),
+//      floatingActionButton: new FloatingActionButton(
+//        onPressed: _handleShowSettings,
+//        tooltip: 'Increment',
+//        child: new Icon(Icons.arrow_forward),
+//      ),
     );
   }
 }
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+enum _QuestionMenuItem { how, about }
+
 class SettingsPage extends StatefulWidget {
   SettingsPage({Key key, this.title}) : super(key: key);
 
@@ -11,9 +13,37 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
 
+  void _handleMenu(BuildContext context, _QuestionMenuItem value) {
+    //TODO
+  }
+
+
+  Widget _buildAppBar() {
+    return new AppBar(
+        elevation: 0,
+        backgroundColor: new Color(0xFF1c283d),
+        actions: <Widget>[
+          new PopupMenuButton<_QuestionMenuItem>(
+            onSelected: (_QuestionMenuItem value) {
+              _handleMenu(context, value);
+            },
+            itemBuilder: (BuildContext context) =>
+            <PopupMenuItem<_QuestionMenuItem>>[
+              new PopupMenuItem<_QuestionMenuItem>(
+                  value: _QuestionMenuItem.how,
+                  child: const Text('How this works')),
+              new PopupMenuItem<_QuestionMenuItem>(
+                  value: _QuestionMenuItem.about,
+                  child: const Text('About'))
+            ],
+          ),
+        ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      appBar: _buildAppBar(),
       body: new Center(
         child: new Text(
           'Settings Panel',
